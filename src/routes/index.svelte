f04012cdf043c9f7fcf2360fea8b7ee3452a9551
Lukas Jørgensen <lukas.dinven@gmail.com>
2022-06-29 14:13:11 +0200

    .

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,7 +5,7 @@
 <div class="grid justify-center items-center my-96 text-center gap-5">
     <h1 class="text-5xl font-semibold">SvelteKit Tailwindcss DaisyUI Iconify</h1>
     <div>
-        <a href="https://tailwindcss.com" class="btn btn-primary mx-10">Click me</a>
-        <a href="https://daisyui.com" class="btn btn-error mx-10">Not me</a>
+        <a href="https://tailwindcss.com" target="_blank" class="btn btn-info mx-10">Click me</a>
+        <a href="https://daisyui.com" target="_blank" class="btn btn-warning mx-10">Not me</a>
     </div>
 </div>
